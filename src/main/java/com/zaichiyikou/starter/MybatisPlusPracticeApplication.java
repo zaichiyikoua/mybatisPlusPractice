fce06e763ad47c3c2eab0b6737eea4ba2492697f
zaichiyikou <913475430@qq.com>
2020-04-13 21:35:54 +0800

    初始化项目

---
+++ src/main/java/com/zaichiyikou/starter/MybatisPlusPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.zaichiyikou.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisPlusPracticeApplication.class, args);
+	}
+
+}
